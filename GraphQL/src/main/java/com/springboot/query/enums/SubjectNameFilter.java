07828886c08dec362e60139d37ccaeb4682e1639
khasakinil <khasakinil@gmail.com>
2024-06-26 22:50:25 +0530

    Edge and Filter

---
+++ GraphQL/src/main/java/com/springboot/query/enums/SubjectNameFilter.java
@@ -0,0 +1,5 @@
+package com.springboot.query.enums;
+
+public enum SubjectNameFilter {
+	All, Java, MySQL, MongoDB
+}
--- GraphQL/src/main/java/com/springboot/query/resolver/StudentResponseResolver.java
+++ GraphQL/src/main/java/com/springboot/query/resolver/StudentResponseResolver.java
@@ -7,18 +7,24 @@ import org.springframework.stereotype.Service;
 
 import com.coxautodev.graphql.tools.GraphQLResolver;
 import com.springboot.query.entity.Subject;
+import com.springboot.query.enums.SubjectNameFilter;
 import com.springboot.query.response.StudentResponse;
 import com.springboot.query.response.SubjectResponse;
 
 @Service
 public class StudentResponseResolver implements GraphQLResolver<StudentResponse> {
-	public List<SubjectResponse> getLearningSubjects(StudentResponse studentResponse) {
+	public List<SubjectResponse> getLearningSubjects(StudentResponse studentResponse,
+			SubjectNameFilter subjectNameFilter) {
 
 		List<SubjectResponse> learningSubjects = new ArrayList<SubjectResponse>();
 		if (studentResponse.getStudent().getLearningSubjects() != null) {
-
 			for (Subject subject : studentResponse.getStudent().getLearningSubjects()) {
-				learningSubjects.add(new SubjectResponse(subject));
+				if (subjectNameFilter.name().equalsIgnoreCase("All")) {
+					learningSubjects.add(new SubjectResponse(subject));
+				}
+				else if (subjectNameFilter.name().equalsIgnoreCase(subject.getSubjectName())) {
+					learningSubjects.add(new SubjectResponse(subject));
+				}
 			}
 		}
 		return learningSubjects;
